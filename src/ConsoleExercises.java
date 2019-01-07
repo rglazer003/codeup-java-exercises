1bc0c12e8d9cb44339cf8dac26b8adceb93d48b6
Ryan Glazer <rglazer003@gmail.com>
2019-01-07 16:57:49 -0600

    Up to perimiter

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is %.2f%n", pi);
+//        System.out.println("Please enter a number.");
+//        int number = scanner.nextInt();
+//        System.out.println(number);
+//        System.out.println("Please enter three words.");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//        System.out.println(word1 + " "+ word2 + " " + word3);
+        System.out.println("Please type a sentence.");
+        String sent = scanner.nextLine();
+        System.out.println(sent);
+
+    }
+}
