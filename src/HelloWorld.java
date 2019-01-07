dca5c6378efdff7b8a303b87ca1e655d73e64168
Ryan Glazer <rglazer003@gmail.com>
2019-01-07 14:34:03 -0600

    Finished Syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,20 @@ public class HelloWorld {
     public static void main (String[] args){
         System.out.print("Hello, World!");
         System.out.print('\n');
+        int myFavoriteNumber = 11;
+        System.out.println(myFavoriteNumber);
+        String myString = "This is a string";
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+        int max = Integer.MAX_VALUE;
+        System.out.println(max);
+        System.out.println(++max);
     }
 }
