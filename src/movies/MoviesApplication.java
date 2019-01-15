9bf29fc6733e205eb641487c077a87e703ee465c
Ryan Glazer <rglazer003@gmail.com>
2019-01-15 15:42:54 -0600

    Movie bonus completed

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -7,22 +7,21 @@ import java.util.Scanner;
 public class MoviesApplication {
     public static void main(String[] args) {
         boolean loop;
+        Movie [] listAll = MoviesArray.findAll();
         do {
             listOptions();
-            int choice = Input.getIntMovie(0,5);
+            int choice = Input.getIntMovie(0,6);
             Input.waitForEnter();
             if (choice == 0){
                 break;
             }
             if (choice == 1){
                 int list = 1;
-                Movie [] listAll = MoviesArray.findAll();
                 listMovies(listAll);
             }
             if (choice == 2){
                 int list = 1;
                 int count = 0;
-                Movie [] listAll = MoviesArray.findAll();
                 for (Movie movie: listAll){
                     String check = movie.getCategory();
                     if (check.equalsIgnoreCase("animated")){
@@ -43,7 +42,6 @@ public class MoviesApplication {
             if (choice == 3) {
                 int list = 1;
                 int count = 0;
-                Movie [] listAll = MoviesArray.findAll();
                 for (Movie movie: listAll){
                     String check = movie.getCategory();
                     if (check.equalsIgnoreCase("drama")){
@@ -64,7 +62,6 @@ public class MoviesApplication {
             if (choice == 4){
                 int list = 1;
                 int count = 0;
-                Movie [] listAll = MoviesArray.findAll();
                 for (Movie movie: listAll){
                     String check = movie.getCategory();
                     if (check.equalsIgnoreCase("horror")){
@@ -85,7 +82,6 @@ public class MoviesApplication {
             if (choice == 5){
                 int list = 1;
                 int count = 0;
-                Movie [] listAll = MoviesArray.findAll();
                 for (Movie movie: listAll){
                     String check = movie.getCategory();
                     if (check.equalsIgnoreCase("scifi")){
@@ -103,6 +99,23 @@ public class MoviesApplication {
                 }
                 listMovies(scifiList);
             }
+            if (choice == 6){
+                int length = listAll.length;
+                System.out.println("Enter name");
+                String name = Input.getString();
+                System.out.println("Enter category");
+                String category = Input.getString();
+                Movie [] newList = new Movie[length+1];
+                int i = 0;
+                for (Movie movie: listAll){
+                    newList[i]=movie;
+                    i++;
+                }
+                Movie newMovie = new Movie(name,category);
+                newList[length]=newMovie;
+                listMovies(newList);
+                listAll=newList;
+            }
             System.out.println("Press enter to continue");
             Input.waitForEnter();
             loop = Input.yesNo("Continue?");
@@ -112,7 +125,7 @@ public class MoviesApplication {
     }
     public static void listOptions(){
         System.out.println("What would you like to do?\n0. Exit\n1. View all movies\n2. View Animated movies\n" +
-                "3. View Drama movies\n4. View Horror movies\n5. View Scifi movies");
+                "3. View Drama movies\n4. View Horror movies\n5. View Scifi movies\n6. Add a movie");
     }
     public static void listMovies(Movie movies[]){
         int list = 1;
